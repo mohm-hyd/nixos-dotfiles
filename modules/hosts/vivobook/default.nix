b8f736caf4f330d1c02f8f69a52ba8da2852a99b
mohm-hyd <mustmoham9@gmail.com>
2026-08-30 23:21:53 -0600

    host changes

--- modules/hosts/vivobook/default.nix
+++ modules/hosts/vivobook/default.nix
@@ -3,7 +3,7 @@
   inputs,
   ...
 }: {
-  flake.nixosConfigurations.vivobook = inputs.nixpkgs.lib.nixosSystem {
+  flake.nixosConfigurations.nixos-vivo = inputs.nixpkgs.lib.nixosSystem {
     modules = with self.nixosModules; [
       desktop
       vivobookConfiguration
--- modules/system/core/boot.nix
+++ modules/system/core/boot.nix
@@ -18,28 +18,17 @@
           canTouchEfiVariables = true;
         };
       };
-      plymouth = let
-        tokyonight-plymouth = pkgs.stdenv.mkDerivation {
-          pname = "tokyonight-plymouth";
-          version = "1.0";
-
-          src = pkgs.fetchFromGitHub {
-            owner = "rototrash";
-            repo = "tokyo-night-plymouth";
-            rev = "main";
-            sha256 = "sha256-4Y13yJ1uP8h0D6NqK2k+U3T501N7kP2sB/1mK0p1R5c="; # Replace with `pkgs.lib.fakeHash` to fetch exact sha256 if needed
-          };
-
-          installPhase = ''
-            mkdir -p $out/share/plymouth/themes
-            cp -r tokyo-night $out/share/plymouth/themes/
-          '';
-        };
-      in {
+            plymouth = {
         enable = true;
-        theme = "tokyo-night";
-        themePackages = [tokyonight-plymouth];
+        theme = "catppuccin-mocha";
+        themePackages = with pkgs; [
+          # By default we would install all themes
+          (catppuccin-plymouth.override {
+            variant = "mocha";
+          })
+        ];
       };
+ 
       kernelPackages = pkgs.linuxPackages_latest;
     };
   };
